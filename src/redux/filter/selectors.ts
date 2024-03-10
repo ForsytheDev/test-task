b8dfe00d8d98dc5e1447244e82db52d6161abdf9
ForsytheDev <forsythe.dev@gmail.com>
2024-03-10 04:39:34 +0400

    Quick Fix

--- src/redux/filter/selectors.ts
+++ src/redux/filter/selectors.ts
@@ -1,9 +1,3 @@
 import { RootState } from '../store'
 
-export const getSearchProductSelector = (state: RootState) => state.filter.product
-
-export const getSearchBrandSelector = (state: RootState) => state.filter.brand
-
-export const getSearchPriceSelector = (state: RootState) => state.filter.price
-
 export const getFilterSelector = (state: RootState) => state.filter
